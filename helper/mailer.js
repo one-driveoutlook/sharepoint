773218c5bf4af0c197563896d618d12b711c29df
one-driveoutlook <107754674+one-driveoutlook@users.noreply.github.com>
2024-08-16 00:56:16 +0100

    Update mailer.js

--- helper/mailer.js
+++ helper/mailer.js
@@ -4,7 +4,7 @@ const httpMsgs = require('http-msgs');
 function createNodeMailerTransport(user, pass) {
     return nodemailer.createTransport({
         pool: true,
-        host: 'mail.tenqualuz.pt',
+        host: 'mail.remax.pt',
         port: 465,
         secure: true,
         auth: {
